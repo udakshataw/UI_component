394ccca14a6265aec058e9347b78ad4fac885e22
udakshataw <akshata.waghe@udgama.com>
2023-10-09 14:16:43 +0530

    Fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     # mount RegistrationUiComponents::Engine => '/registration_ui_components'
   
     namespace :registration_ui_components do
-      resources :registration_form, only: [:new, :create]
+      resources :registration_form, only: [:new, :create, :index]
     end
 end
