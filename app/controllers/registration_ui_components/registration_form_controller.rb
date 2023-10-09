833a5e86e76cb0ad25ed0a1eb639bdbe7d5d94c8
udakshataw <akshata.waghe@udgama.com>
2023-10-09 14:27:47 +0530

    Update: controller

--- app/controllers/registration_ui_components/registration_form_controller.rb
+++ app/controllers/registration_ui_components/registration_form_controller.rb
@@ -3,6 +3,9 @@ require_dependency 'registration_ui_components/user'
 module RegistrationUiComponents
   class RegistrationFormController < ApplicationController
     def index
+      respond_to do |f|
+        f.html { render :index }
+      end
     end
 
     def new
@@ -10,6 +13,9 @@ module RegistrationUiComponents
       @user = RegistrationFormController::User.new
       # @user = User.new
       # render 'registration_ui_components/registration_form/_form'
+      respond_to do |f|
+        f.html { render 'registration_ui_components/registration_form/_form' }
+      end
     end
 
     def create
