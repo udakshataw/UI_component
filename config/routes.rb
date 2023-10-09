f0aa34d5fcc90b0be553f56fb830c14bea9b0bbd
udakshataw <akshata.waghe@udgama.com>
2023-10-09 14:39:37 +0530

    Fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     # mount RegistrationUiComponents::Engine => '/registration_ui_components'
   
     namespace :registration_ui_components do
-      resources :registration_form, only: [:new, :create, :index]
+    #   resources :registration_form, only: [:new, :create, :index]
+    get '/new', to: 'registration_form#new'
+    get '/', to: 'registration_form#index'
+    post '/', to: 'registration_form#create', as: 'form_submission'
     end
 end
