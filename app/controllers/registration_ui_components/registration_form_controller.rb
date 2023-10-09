15bcc8440c5eaab02e306676f68b2bc9c2b9fe5b
udakshataw <akshata.waghe@udgama.com>
2023-10-09 14:31:28 +0530

    Fix: render path

--- app/controllers/registration_ui_components/registration_form_controller.rb
+++ app/controllers/registration_ui_components/registration_form_controller.rb
@@ -1,16 +1,16 @@
-require_dependency 'registration_ui_components/user'
+# require_dependency 'registration_ui_components/user'
 
 module RegistrationUiComponents
   class RegistrationFormController < ApplicationController
     def index
       respond_to do |f|
-        f.html { render :index }
+        f.html { render 'registration_ui_components/registration_form/index' }
       end
     end
 
     def new
       puts "Inside registration new definition!"
-      @user = RegistrationFormController::User.new
+      # @user = RegistrationFormController::User.new
       # @user = User.new
       # render 'registration_ui_components/registration_form/_form'
       respond_to do |f|
