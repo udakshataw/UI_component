567e24190ab14f6c56cdc045524f0252ab5108c4
udakshataw <akshata.waghe@udgama.com>
2023-10-09 12:31:08 +0530

    Fix: controller

--- app/controllers/registration_ui_components/registration_form_controller.rb
+++ app/controllers/registration_ui_components/registration_form_controller.rb
@@ -2,7 +2,7 @@ module RegistrationUiComponents
   class RegistrationFormController < ApplicationController
     def new
       puts "Inside registration new definition!"
-      @user = RegistrationFormController::User.new
+      @user = User.new
       # render 'registration_ui_components/registration_form/_form'
     end
 
