44e73f5ea1ed07eb245b4bac466bbbd32bc1aace
udakshataw <akshata.waghe@udgama.com>
2023-10-09 11:08:33 +0530

    Fix: rendering of form

--- app/controllers/my_ui_components/my_component_controller.rb
+++ app/controllers/my_ui_components/my_component_controller.rb
@@ -2,6 +2,7 @@ module MyUiComponents
   class MyComponentController < ApplicationController
     def new
       @my_model = MyComponentModel.new
+      render 'my_ui_components/my_component/_form'
     end
 
     def create
